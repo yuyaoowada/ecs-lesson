d43e202b10c10903c4f7783f205e07ee19d6c507
yuyaoowada <yuyaoowada@gmail.com>
2023-10-30 01:47:14 +0900

    update version

--- sample-webapp/tests/unit/example.spec.js
+++ sample-webapp/tests/unit/example.spec.js
@@ -3,13 +3,15 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg =
-      "This application is Sample WebApp for CICD lesson. Response from REST API is...";
+    const testmsg =
+      "This application is Sample WebApp for CICD lesson.(version1.0.1) Response from REST API is...";
 
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+      props: {
+        msg: "This application is Sample WebApp for CICD lesson.(version1.0.1) ",
+      },
     });
 
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(testmsg);
   });
 });
