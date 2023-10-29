ae2fd341dc1946ea5c84d95404eed3639042e6ca
yuyaoowada <yuyaoowada@gmail.com>
2023-10-29 22:38:32 +0900

    mod package.json use first sourse

--- sample-webapp/tests/unit/example.spec.js
+++ sample-webapp/tests/unit/example.spec.js
@@ -4,10 +4,12 @@ import HelloWorld from "@/components/HelloWorld.vue";
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg =
-      "This application is Sample WebApp for CICD lesson.Response from REST API is...";
+      "This application is Sample WebApp for CICD lesson. Response from REST API is...";
+
     const wrapper = shallowMount(HelloWorld, {
       props: { msg },
     });
+
     expect(wrapper.text()).toMatch(msg);
   });
 });
